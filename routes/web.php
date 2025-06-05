28438720ef9ce7419abbc37807b59c9c36aa9022
Alex <alex_p97@live.com>
2025-06-05 22:20:10 +0100

    Created route for edit form

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\TaskController;
 // API Endpoints for Task Management
 Route::get('/tasks', [TaskController::class, 'index']);             // Show all tasks
 Route::get('/tasks/create', [TaskController::class, 'create']);     // Show task creation form
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit']);    // Show task edit form
 Route::post('/tasks', [TaskController::class, 'store']);            // Handle new task submission
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);   // Delete a task
