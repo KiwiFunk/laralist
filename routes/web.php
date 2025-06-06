7cdbeafc27b9cb3289bf72f625732ab005b1bcf3
Alex <alex_p97@live.com>
2025-06-06 01:32:53 +0100

    Created patch route

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\TaskController;
 Route::get('/', function () {
     return view('welcome'); // Show the welcome page
 });
-Route::get('/tasks', [TaskController::class, 'index']);             // Show all tasks
-Route::get('/tasks/create', [TaskController::class, 'create']);     // Show task creation form
-Route::get('/tasks/{id}/edit', [TaskController::class, 'edit']);    // Show task edit form
-Route::post('/tasks', [TaskController::class, 'store']);            // Handle new task submission
-Route::put('/tasks/{id}', [TaskController::class, 'update']);       // Update an existing task
-Route::delete('/tasks/{id}', [TaskController::class, 'delete']);   // Delete a task
+Route::get('/tasks', [TaskController::class, 'index']);                 // Show all tasks
+Route::get('/tasks/create', [TaskController::class, 'create']);         // Show task creation form
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit']);        // Show task edit form
+Route::post('/tasks', [TaskController::class, 'store']);                // Handle new task submission
+Route::put('/tasks/{id}', [TaskController::class, 'update']);           // Update an existing task
+Route::patch('/tasks/{id}', [TaskController::class, 'toggleStatus']);   // Partial update to toggle task status
+Route::delete('/tasks/{id}', [TaskController::class, 'delete']);        // Delete a task
