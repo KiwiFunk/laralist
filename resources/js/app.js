6b27e6f350503c839f5ec1b62de0088e2a18f7ee
Alex <alex_p97@live.com>
2025-06-07 22:09:34 +0100

    Updated app.js to include new alpine modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,20 @@
 import './bootstrap';
 import Alpine from 'alpinejs'
- 
+
+// Import Modules
+import { createTaskStore } from './alpine/store';
+import { taskCard } from './alpine/components/taskCard';
+import { createTaskForm } from './alpine/components/createTask';
+
+// Register Alpine stores and components
+document.addEventListener('alpine:init', () => {
+
+    Alpine.data('taskCard', taskCard);
+    Alpine.data('createTaskForm', createTaskForm);
+    
+    // Store will be initialized in the Blade template with server data
+});
+
 window.Alpine = Alpine
  
 Alpine.start()
