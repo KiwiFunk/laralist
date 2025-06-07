da3e7eddb2b339fdede5e68d4cd20844d97dc411
Alex <alex_p97@live.com>
2025-06-07 22:58:55 +0100

    Moved styles into app.css

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -34,28 +34,5 @@
         @endif
     </div>
 
-    <style>
-        @keyframes slideInUp {
-            from {
-                opacity: 0;
-                transform: translateY(30px);
-            }
-            to {
-                opacity: 1;
-                transform: translateY(0);
-            }
-        }
-
-        .deleting {
-                animation: slideOut 0.3s ease-out forwards !important;
-            }
-            
-        @keyframes slideOut {
-            to {     
-                transform: translateX(100%);
-                opacity: 0;
-            }
-        }
-    </style>
 </body>
 </html>
