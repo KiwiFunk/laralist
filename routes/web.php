096da38525361a65f598644579bce2f9a6a7a816
Alex <alex_p97@live.com>
2025-07-07 18:40:51 +0100

    Imported Project controller in routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 // Import Controllers
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
