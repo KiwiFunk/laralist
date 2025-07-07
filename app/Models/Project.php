f4ef8616cbb8e2371d6e610ae4182584bf8266c7
Alex <alex_p97@live.com>
2025-07-07 13:35:25 +0100

    Created project model

---
+++ app/Models/Project.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace All\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'user_id',          // Foreign key to the user who owns the project
+        'name',             // Project name (e.g., "Home Renovation")
+        'description',      // Optional project details
+        'created_on',       // Timestamp when the project was created
+        'tasks',            // Array of tasks associated with the project
+    ];
+
+    // Database relationships
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
