ec2ebd2a94b3bc9274626c4d890c2b8308cbbf09
Alex <alex_p97@live.com>
2025-07-08 13:13:46 +0100

    Updated Project Controller to pass project data for user on GET request

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -12,7 +12,7 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Auth::user()->projects();
-        return view('index', compact('projects'));
+        return view('projectsindex', ['projects' => $projects]);
     }
 
     // Store a new Project
