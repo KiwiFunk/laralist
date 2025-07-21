81e37c4df1d433441e5be7cbd7a1c7ad6d07946e
Alex <alex_p97@live.com>
2025-07-21 14:17:23 +0100

    Updated routes to use new parameter binding

--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,17 @@ Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 Route::middleware(['auth', 'throttle:100,1'])->group(function () {
     
     // Project routes
-    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');               // Get all projects
-    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');              // Handle project creation
-    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');       // Get specified project
-    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-    Route::delete('/projects/{project}', [ProjectController::class, 'delete'])->name('projects.destroy');
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');                   // Get all projects
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');                  // Handle project creation
+    Route::get('/projects/{project:slug}', [ProjectController::class, 'show'])->name('projects.show');      // Get specified project
+    Route::put('/projects/{project:slug}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project:slug}', [ProjectController::class, 'delete'])->name('projects.destroy');
     // Note: Deleting a project will also delete all associated tasks due to cascade delete in the model
     
     // Task routes (nested under projects)
-    Route::post('/projects/{project}/tasks', [TaskController::class, 'store'])->name('tasks.store');    // Handle new task submission
-    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');               // Update an existing task
-    Route::post('/tasks/{task}/toggle', [TaskController::class, 'toggleStatus'])->name('tasks.toggle'); // Toggle task status
-    Route::delete('/tasks/{task}', [TaskController::class, 'delete']);                                    // Delete a task
+    Route::post('/projects/{project:slug}/tasks', [TaskController::class, 'store'])->name('tasks.store');   // Handle new task submission
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');                   // Update an existing task
+    Route::post('/tasks/{task}/toggle', [TaskController::class, 'toggleStatus'])->name('tasks.toggle');     // Toggle task status
+    Route::delete('/tasks/{task}', [TaskController::class, 'delete']);                                      // Delete a task
     
 });
